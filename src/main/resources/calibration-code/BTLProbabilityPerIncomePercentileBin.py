5e0d7f7a34c43cb3f25634679559e365ed777cc2
ms4623 <ms4623@ic.ac.uk>
2025-12-30 20:06:46 +0000

    feat [MS]: Use WAS Constants in BTLProbabilityPerIncomePercentileBin.py and format it

--- src/main/resources/calibration-code/BTLProbabilityPerIncomePercentileBin.py
+++ src/main/resources/calibration-code/BTLProbabilityPerIncomePercentileBin.py
@@ -3,12 +3,32 @@
 Class to study the probability of a household becoming a buy-to-let investor depending on its income percentile, based
 on Wealth and Assets Survey data. This is the code used to create the file "BTLProbabilityPerIncomePercentileBin.csv".
 
-@author: Adrian Carro
+@author: Adrian Carro, Max Stoddard
 """
 
 from __future__ import division
+import os
+import sys
+
 import pandas as pd
 from scipy import stats
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from WealthAssetsSurveyConstants import (
+    WAS_COLUMN_MAP,
+    WAS_COLUMN_RENAME_MAP,
+    WAS_DATA_FILENAME,
+    WAS_DATA_SEPARATOR,
+    WAS_WEIGHT,
+    WAS_NET_ANNUAL_INCOME,
+    WAS_GROSS_ANNUAL_INCOME,
+    WAS_NET_ANNUAL_RENTAL_INCOME,
+    WAS_GROSS_ANNUAL_RENTAL_INCOME,
+)
+
+GROSS_NON_RENT_INCOME = "GrossNonRentIncome"
+NET_NON_RENT_INCOME = "NetNonRentIncome"
+GROSS_NON_RENT_INCOME_PERCENTILE = "GrossNonRentIncomePercentile"
 
 # List of household variables currently used
 # DVTotGIRw3                  Household Gross Annual (regular) income
@@ -18,41 +38,62 @@ from scipy import stats
 
 # Read Wealth and Assets Survey data for households
 root = r""  # ADD HERE PATH TO WAS DATA FOLDER
-chunk = pd.read_csv(root + r"/was_wave_3_hhold_eul_final.dta", usecols={"w3xswgt", "DVTotGIRw3", "DVTotNIRw3",
-                                                                             "DVGrsRentAmtAnnualw3_aggr",
-                                                                             "DVNetRentAmtAnnualw3_aggr"})
-pd.set_option('display.max_columns', None)
+use_columns = [
+    WAS_WEIGHT,
+    WAS_GROSS_ANNUAL_INCOME,
+    WAS_NET_ANNUAL_INCOME,
+    WAS_GROSS_ANNUAL_RENTAL_INCOME,
+    WAS_NET_ANNUAL_RENTAL_INCOME,
+]
+chunk = pd.read_csv(
+    os.path.join(root, WAS_DATA_FILENAME),
+    usecols=[WAS_COLUMN_MAP[column] for column in use_columns],
+    sep=WAS_DATA_SEPARATOR,
+)
+pd.set_option("display.max_columns", None)
 
 # Rename columns to be used and add all necessary extra columns
-chunk.rename(columns={"w3xswgt": "Weight"}, inplace=True)
-chunk.rename(columns={"DVTotGIRw3": "GrossTotalIncome"}, inplace=True)
-chunk.rename(columns={"DVTotNIRw3": "NetTotalIncome"}, inplace=True)
-chunk.rename(columns={"DVGrsRentAmtAnnualw3_aggr": "GrossRentalIncome"}, inplace=True)
-chunk.rename(columns={"DVNetRentAmtAnnualw3_aggr": "NetRentalIncome"}, inplace=True)
-chunk["GrossNonRentIncome"] = chunk["GrossTotalIncome"] - chunk["GrossRentalIncome"]
-chunk["NetNonRentIncome"] = chunk["NetTotalIncome"] - chunk["NetRentalIncome"]
+chunk.rename(columns=WAS_COLUMN_RENAME_MAP, inplace=True)
+chunk[GROSS_NON_RENT_INCOME] = (
+    chunk[WAS_GROSS_ANNUAL_INCOME] - chunk[WAS_GROSS_ANNUAL_RENTAL_INCOME]
+)
+chunk[NET_NON_RENT_INCOME] = (
+    chunk[WAS_NET_ANNUAL_INCOME] - chunk[WAS_NET_ANNUAL_RENTAL_INCOME]
+)
 
 # Filter out the 1% with highest GrossTotalIncome and the 1% with lowest NetTotalIncome
 one_per_cent = int(round(len(chunk.index) / 100))
-chunk_ord_by_net = chunk.sort_values("NetTotalIncome")
-chunk_ord_by_gross = chunk.sort_values("GrossTotalIncome")
-min_net_total_income = chunk_ord_by_net.iloc[one_per_cent]["NetTotalIncome"]
-max_gross_total_income = chunk_ord_by_gross.iloc[-one_per_cent]["GrossTotalIncome"]
-chunk = chunk[chunk["NetTotalIncome"] >= min_net_total_income]
-chunk = chunk[chunk["GrossTotalIncome"] <= max_gross_total_income]
+chunk_ord_by_net = chunk.sort_values(WAS_NET_ANNUAL_INCOME)
+chunk_ord_by_gross = chunk.sort_values(WAS_GROSS_ANNUAL_INCOME)
+min_net_total_income = chunk_ord_by_net.iloc[one_per_cent][WAS_NET_ANNUAL_INCOME]
+max_gross_total_income = chunk_ord_by_gross.iloc[-one_per_cent][WAS_GROSS_ANNUAL_INCOME]
+chunk = chunk[chunk[WAS_NET_ANNUAL_INCOME] >= min_net_total_income]
+chunk = chunk[chunk[WAS_GROSS_ANNUAL_INCOME] <= max_gross_total_income]
 
 # Compute income percentiles (using gross non-rent income) of all households
-chunk["GrossNonRentIncomePercentile"] = [stats.percentileofscore(chunk["GrossNonRentIncome"].values, x, "weak")
-                                         for x in chunk["GrossNonRentIncome"]]
+chunk[GROSS_NON_RENT_INCOME_PERCENTILE] = [
+    stats.percentileofscore(chunk[GROSS_NON_RENT_INCOME].values, x, "weak")
+    for x in chunk[GROSS_NON_RENT_INCOME]
+]
 
 # Write to file probability of being a BTL investor for each percentile bin (of width 1%)
 with open("BTLProbabilityPerIncomePercentileBin.csv", "w") as f:
-    f.write("# Gross non-rental income percentile (lower edge), gross non-rental income percentile (upper edge), "
-            "BTL probability\n")
+    f.write(
+        "# Gross non-rental income percentile (lower edge), gross non-rental income percentile (upper edge), "
+        "BTL probability\n"
+    )
     for a in range(100):
-        n_total = len(chunk[(a < chunk["GrossNonRentIncomePercentile"])
-                            & (chunk["GrossNonRentIncomePercentile"] <= a + 1.0)])
-        n_BTL = len(chunk[(a < chunk["GrossNonRentIncomePercentile"])
-                          & (chunk["GrossNonRentIncomePercentile"] <= a + 1.0)
-                          & (chunk["GrossRentalIncome"] > 0.0)])
-        f.write("{}, {}, {}\n".format(a/100, (a + 1)/100, n_BTL/n_total))
+        n_total = len(
+            chunk[
+                (a < chunk[GROSS_NON_RENT_INCOME_PERCENTILE])
+                & (chunk[GROSS_NON_RENT_INCOME_PERCENTILE] <= a + 1.0)
+            ]
+        )
+        n_BTL = len(
+            chunk[
+                (a < chunk[GROSS_NON_RENT_INCOME_PERCENTILE])
+                & (chunk[GROSS_NON_RENT_INCOME_PERCENTILE] <= a + 1.0)
+                & (chunk[WAS_GROSS_ANNUAL_RENTAL_INCOME] > 0.0)
+            ]
+        )
+        f.write("{}, {}, {}\n".format(a / 100, (a + 1) / 100, n_BTL / n_total))
