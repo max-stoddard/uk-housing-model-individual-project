b63e2e4a49ec598aa76b9ff18055d4d4880dc9ef
ms4623 <ms4623@ic.ac.uk>
2025-12-30 20:10:06 +0000

    feat [MS]: Use WAS constants in TotalWealthDist.py, format

--- src/main/resources/validation-code/TotalWealthDist.py
+++ src/main/resources/validation-code/TotalWealthDist.py
@@ -3,42 +3,102 @@
 Class to study households' total wealth (financial + housing) distribution, for validation purposes, based on Wealth and
 Assets Survey data.
 
-@author: Adrian Carro
+@author: Adrian Carro, Max Stoddard
 """
 
 from __future__ import division
+import os
+import sys
+
 import pandas as pd
 import numpy as np
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from WealthAssetsSurveyConstants import (
+    WAS_COLUMN_MAP,
+    WAS_DATA_FILENAME,
+    WAS_DATA_SEPARATOR,
+    WAS_WEIGHT,
+    WAS_GROSS_FINANCIAL_WEALTH,
+    WAS_NET_FINANCIAL_WEALTH,
+    WAS_NATIONAL_SAVINGS_VALUE,
+    WAS_CHILD_TRUST_FUND_VALUE,
+    WAS_CHILD_OTHER_SAVINGS_VALUE,
+    WAS_SAVINGS_ACCOUNTS_VALUE,
+    WAS_CASH_ISA_VALUE,
+    WAS_CURRENT_ACCOUNT_CREDIT_VALUE,
+    WAS_TOTAL_PROPERTY_WEALTH,
+    WAS_PROPERTY_VALUE_SUM,
+    WAS_MAIN_RESIDENCE_VALUE,
+    WAS_OTHER_HOUSES_TOTAL_VALUE,
+    WAS_BTL_HOUSES_TOTAL_VALUE,
+)
+
+# Make class constants for calculated columns
+WAS_LIQ_FINANCIAL_WEALTH = "LiqFinancialWealth"
+WAS_GROSS_HOUSING_WEALTH = "GrossHousingWealth"
+WAS_TOTAL_WEALTH = "TotalWealth"
 
 
 # Read Wealth and Assets Survey data for households
 root = r""  # ADD HERE PATH TO WAS DATA FOLDER
-chunk = pd.read_csv(root + r"/was_wave_3_hhold_eul_final.dta", usecols={"w3xswgt", "HFINWW3_sum", "HFINWNTW3_sum",
-                                                                        "DVFNSValW3_aggr", "DVCACTvW3_aggr",
-                                                                        "DVCASVVW3_aggr", "DVSaValW3_aggr",
-                                                                        "DVCISAVW3_aggr", "DVCaCrValW3_aggr",
-                                                                        "HPROPWW3", "DVPropertyW3", "DVHValueW3",
-                                                                        "DVHseValW3_sum", "DVBltValW3_sum"})
+use_column_constants = [
+    WAS_WEIGHT,
+    WAS_GROSS_FINANCIAL_WEALTH,
+    WAS_NET_FINANCIAL_WEALTH,
+    WAS_NATIONAL_SAVINGS_VALUE,
+    WAS_CHILD_TRUST_FUND_VALUE,
+    WAS_CHILD_OTHER_SAVINGS_VALUE,
+    WAS_SAVINGS_ACCOUNTS_VALUE,
+    WAS_CASH_ISA_VALUE,
+    WAS_CURRENT_ACCOUNT_CREDIT_VALUE,
+    WAS_TOTAL_PROPERTY_WEALTH,
+    WAS_PROPERTY_VALUE_SUM,
+    WAS_MAIN_RESIDENCE_VALUE,
+    WAS_OTHER_HOUSES_TOTAL_VALUE,
+    WAS_BTL_HOUSES_TOTAL_VALUE,
+]
+use_columns = [WAS_COLUMN_MAP[column] for column in use_column_constants]
+chunk = pd.read_csv(
+    os.path.join(root, WAS_DATA_FILENAME),
+    usecols=use_columns,
+    sep=WAS_DATA_SEPARATOR,
+)
 
 # List of household variables currently used
 # HFINWW3_sum                   Gross Financial Wealth (financial assets only)
 # HFINWNTW3_sum                 Household Net financial Wealth (financial assets minus financial liabilities)
 
-# Rename the different measures of financial wealth
-chunk.rename(columns={"w3xswgt": "Weight"}, inplace=True)
-chunk.rename(columns={"HFINWW3_sum": "GrossFinancialWealth"}, inplace=True)
-chunk.rename(columns={"HFINWNTW3_sum": "NetFinancialWealth"}, inplace=True)
-chunk["LiqFinancialWealth"] = chunk["DVFNSValW3_aggr"].astype(float) + chunk["DVCACTvW3_aggr"].astype(float)\
-                           + chunk["DVCASVVW3_aggr"].astype(float) + chunk["DVSaValW3_aggr"].astype(float)\
-                           + chunk["DVCISAVW3_aggr"].astype(float) + chunk["DVCaCrValW3_aggr"].astype(float)
-# Rename the different measures of housing wealth
-chunk.rename(columns={"HPROPWW3": "NetPropertyWealth"}, inplace=True)
-chunk.rename(columns={"DVPropertyW3": "GrossPropertyWealth"}, inplace=True)
-chunk["GrossHousingWealth"] = chunk["DVHValueW3"].astype(float) + chunk["DVHseValW3_sum"].astype(float)\
-                           + chunk["DVBltValW3_sum"].astype(float)
+# Rename columns to their constant names
+chunk.rename(
+    columns={WAS_COLUMN_MAP[column]: column for column in use_column_constants},
+    inplace=True,
+)
+chunk[WAS_LIQ_FINANCIAL_WEALTH] = (
+    chunk[WAS_NATIONAL_SAVINGS_VALUE].astype(float)
+    + chunk[WAS_CHILD_TRUST_FUND_VALUE].astype(float)
+    + chunk[WAS_CHILD_OTHER_SAVINGS_VALUE].astype(float)
+    + chunk[WAS_SAVINGS_ACCOUNTS_VALUE].astype(float)
+    + chunk[WAS_CASH_ISA_VALUE].astype(float)
+    + chunk[WAS_CURRENT_ACCOUNT_CREDIT_VALUE].astype(float)
+)
+chunk[WAS_GROSS_HOUSING_WEALTH] = (
+    chunk[WAS_MAIN_RESIDENCE_VALUE].astype(float)
+    + chunk[WAS_OTHER_HOUSES_TOTAL_VALUE].astype(float)
+    + chunk[WAS_BTL_HOUSES_TOTAL_VALUE].astype(float)
+)
 # Filter down to keep only these columns
-chunk = chunk[["Weight", "NetFinancialWealth", "GrossFinancialWealth", "LiqFinancialWealth",
-               "NetPropertyWealth", "GrossPropertyWealth", "GrossHousingWealth"]]
+chunk = chunk[
+    [
+        WAS_WEIGHT,
+        WAS_NET_FINANCIAL_WEALTH,
+        WAS_GROSS_FINANCIAL_WEALTH,
+        WAS_LIQ_FINANCIAL_WEALTH,
+        WAS_TOTAL_PROPERTY_WEALTH,
+        WAS_PROPERTY_VALUE_SUM,
+        WAS_GROSS_HOUSING_WEALTH,
+    ]
+]
 
 # Create wealth bins for histograms
 min_wealth_bin = 2.0
@@ -46,23 +106,50 @@ max_wealth_bin = 16.0
 wealth_bin_edges = np.linspace(min_wealth_bin, max_wealth_bin, 57)
 
 # For each combination of housing wealth and financial wealth measures...
-financial_wealth_measures = ["NetFinancialWealth", "GrossFinancialWealth", "LiqFinancialWealth"]
-housing_wealth_measures = ["NetPropertyWealth", "GrossPropertyWealth", "GrossHousingWealth"]
+financial_wealth_measures = [
+    WAS_NET_FINANCIAL_WEALTH,
+    WAS_GROSS_FINANCIAL_WEALTH,
+    WAS_LIQ_FINANCIAL_WEALTH,
+]
+housing_wealth_measures = [
+    WAS_TOTAL_PROPERTY_WEALTH,
+    WAS_PROPERTY_VALUE_SUM,
+    WAS_GROSS_HOUSING_WEALTH,
+]
 for financial_wealth_measure in financial_wealth_measures:
     for housing_wealth_measure in housing_wealth_measures:
         # ...add total wealth column
-        chunk["TotalWealth"] = chunk[financial_wealth_measure].astype(float)\
-                               + chunk[housing_wealth_measure].astype(float)
+        chunk[WAS_TOTAL_WEALTH] = chunk[financial_wealth_measure].astype(float) + chunk[
+            housing_wealth_measure
+        ].astype(float)
         # For the sake of using logarithmic scales, filter out any zero and negative values
-        temp_chunk = chunk[(chunk["TotalWealth"] > 0.0)]
+        temp_chunk = chunk[(chunk[WAS_TOTAL_WEALTH] > 0.0)]
         # ...create a histogram
-        frequency = np.histogram(np.log(temp_chunk["TotalWealth"].values), bins=wealth_bin_edges, density=True,
-                                 weights=temp_chunk["Weight"].values)[0]
+        frequency = np.histogram(
+            np.log(temp_chunk[WAS_TOTAL_WEALTH].values),
+            bins=wealth_bin_edges,
+            density=True,
+            weights=temp_chunk[WAS_WEIGHT].values,
+        )[0]
         # ...and print the distribution to a file
-        with open(financial_wealth_measure + "-" + housing_wealth_measure + "-Weighted.csv", "w") as f:
-            f.write("# Log Total Wealth (" + financial_wealth_measure + " + " + housing_wealth_measure
-                    + ") (lower edge), Log Total Wealth (" + financial_wealth_measure + " + " + housing_wealth_measure
-                    + ") (upper edge), Probability\n")
-            for element, wealthLowerEdge, wealthUpperEdge in zip(frequency, wealth_bin_edges[:-1],
-                                                                 wealth_bin_edges[1:]):
-                f.write("{}, {}, {}\n".format(wealthLowerEdge, wealthUpperEdge, element))
+        with open(
+            financial_wealth_measure + "-" + housing_wealth_measure + "-Weighted.csv",
+            "w",
+        ) as f:
+            f.write(
+                "# Log Total Wealth ("
+                + financial_wealth_measure
+                + " + "
+                + housing_wealth_measure
+                + ") (lower edge), Log Total Wealth ("
+                + financial_wealth_measure
+                + " + "
+                + housing_wealth_measure
+                + ") (upper edge), Probability\n"
+            )
+            for element, wealthLowerEdge, wealthUpperEdge in zip(
+                frequency, wealth_bin_edges[:-1], wealth_bin_edges[1:]
+            ):
+                f.write(
+                    "{}, {}, {}\n".format(wealthLowerEdge, wealthUpperEdge, element)
+                )
