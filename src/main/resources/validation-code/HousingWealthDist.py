da58d0debe353c236d56a404443bfec8e4ecec99
ms4623 <ms4623@ic.ac.uk>
2025-12-30 20:11:49 +0000

    feat [MS]: Use WAS constants in HousingWealthDist.py, fix reading micro data from files, format

--- src/main/resources/validation-code/HousingWealthDist.py
+++ src/main/resources/validation-code/HousingWealthDist.py
@@ -3,13 +3,31 @@
 Class to study households' housing wealth distribution, for validation purposes, based on Wealth and Assets Survey
 data.
 
-@author: Adrian Carro
+@author: Adrian Carro, Max Stoddard
 """
 
 from __future__ import division
+import os
+import sys
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from WealthAssetsSurveyConstants import (
+    WAS_COLUMN_MAP,
+    WAS_DATA_FILENAME,
+    WAS_DATA_SEPARATOR,
+    WAS_WEIGHT,
+    WAS_TOTAL_PROPERTY_WEALTH,
+    WAS_PROPERTY_VALUE_SUM,
+    WAS_MAIN_RESIDENCE_VALUE,
+    WAS_OTHER_HOUSES_TOTAL_VALUE,
+    WAS_BTL_HOUSES_TOTAL_VALUE,
+)
+
+WAS_GROSS_HOUSING_WEALTH = "GrossHousingWealth"
 
 
 def readResults(file_name, _start_time, _end_time):
@@ -19,29 +37,40 @@ def readResults(file_name, _start_time, _end_time):
     data_float = []
     with open(file_name, "r") as _f:
         for line in _f:
-            if _start_time <= int(line.split(',')[0]) <= _end_time:
-                for column in line.split(',')[1:]:
+            if _start_time <= int(line.split(";")[0]) <= _end_time:
+                for column in line.split(";")[1:]:
                     data_float.append(float(column))
     return data_float
 
 
-# Set control variables and addresses. Note that available variables to print and plot are "NetPropertyWealth",
-# "GrossPropertyWealth", "GrossHousingWealth"
+# Set control variables and addresses. Available variables to print and plot are:
+# WAS_TOTAL_PROPERTY_WEALTH, WAS_PROPERTY_VALUE_SUM, WAS_GROSS_HOUSING_WEALTH
 printResults = False
 plotResults = True
 start_time = 1000
 end_time = 2000
 min_log_bin_edge = 6.0
 max_log_bin_edge = 16.0
-# variableToPlot = "GrossHousingWealth"
-variableToPlot = "NetPropertyWealth"
+# variableToPlot = WAS_GROSS_HOUSING_WEALTH
+variableToPlot = WAS_TOTAL_PROPERTY_WEALTH
 rootData = r""  # ADD HERE PATH TO WAS DATA FOLDER
 rootResults = r""  # ADD HERE PATH TO RESULTS FOLDER
 
 # Read Wealth and Assets Survey data for households
-chunk = pd.read_csv(rootData + r"/was_wave_3_hhold_eul_final.dta", usecols={"w3xswgt", "HPROPWW3", "DVPropertyW3",
-                                                                            "DVHValueW3", "DVHseValW3_sum",
-                                                                            "DVBltValW3_sum"})
+use_column_constants = [
+    WAS_WEIGHT,
+    WAS_TOTAL_PROPERTY_WEALTH,
+    WAS_PROPERTY_VALUE_SUM,
+    WAS_MAIN_RESIDENCE_VALUE,
+    WAS_OTHER_HOUSES_TOTAL_VALUE,
+    WAS_BTL_HOUSES_TOTAL_VALUE,
+]
+use_columns = [WAS_COLUMN_MAP[column] for column in use_column_constants]
+chunk = pd.read_csv(
+    os.path.join(rootData, WAS_DATA_FILENAME),
+    usecols=use_columns,
+    sep=WAS_DATA_SEPARATOR,
+)
 
 # List of household variables currently used
 # HPROPWW3                      Total (net) property wealth (net, i.e., = DVPropertyW3 - HMORTGW3)
@@ -50,17 +79,31 @@ chunk = pd.read_csv(rootData + r"/was_wave_3_hhold_eul_final.dta", usecols={"w3x
 # DVHseValW3_sum                Total value of other houses
 # DVBltValW3_sum                Total value of buy to let houses
 
-# Rename the different measures of housing wealth
-chunk.rename(columns={"w3xswgt": "Weight"}, inplace=True)
-chunk.rename(columns={"HPROPWW3": "NetPropertyWealth"}, inplace=True)
-chunk.rename(columns={"DVPropertyW3": "GrossPropertyWealth"}, inplace=True)
-chunk["GrossHousingWealth"] = chunk["DVHValueW3"].astype(float) + chunk["DVHseValW3_sum"].astype(float)\
-                           + chunk["DVBltValW3_sum"].astype(float)
+# Rename columns to their constant names
+chunk.rename(
+    columns={WAS_COLUMN_MAP[column]: column for column in use_column_constants},
+    inplace=True,
+)
+chunk[WAS_GROSS_HOUSING_WEALTH] = (
+    chunk[WAS_MAIN_RESIDENCE_VALUE].astype(float)
+    + chunk[WAS_OTHER_HOUSES_TOTAL_VALUE].astype(float)
+    + chunk[WAS_BTL_HOUSES_TOTAL_VALUE].astype(float)
+)
 # Filter down to keep only housing wealth
-chunk = chunk[["NetPropertyWealth", "GrossPropertyWealth", "GrossHousingWealth", "Weight"]]
+chunk = chunk[
+    [
+        WAS_TOTAL_PROPERTY_WEALTH,
+        WAS_PROPERTY_VALUE_SUM,
+        WAS_GROSS_HOUSING_WEALTH,
+        WAS_WEIGHT,
+    ]
+]
 # For the sake of using logarithmic scales, filter out any zero and negative values from all wealth columns
-chunk = chunk[(chunk["NetPropertyWealth"] > 0.0) & (chunk["GrossPropertyWealth"] > 0.0) &
-              (chunk["GrossHousingWealth"] > 0.0)]
+chunk = chunk[
+    (chunk[WAS_TOTAL_PROPERTY_WEALTH] > 0.0)
+    & (chunk[WAS_PROPERTY_VALUE_SUM] > 0.0)
+    & (chunk[WAS_GROSS_HOUSING_WEALTH] > 0.0)
+]
 
 # Define bin edges and widths
 number_of_bins = int(max_log_bin_edge - min_log_bin_edge) * 4 + 1
@@ -69,32 +112,76 @@ bin_widths = [b - a for a, b in zip(bin_edges[:-1], bin_edges[1:])]
 
 # If printing data to files is required, histogram data and print results
 if printResults:
-    for variable in ["NetPropertyWealth", "GrossPropertyWealth", "GrossHousingWealth"]:
-        hist = np.histogram(chunk[variable].values, bins=bin_edges, density=True, weights=chunk["Weight"].values)[0]
+    for variable in [
+        WAS_TOTAL_PROPERTY_WEALTH,
+        WAS_PROPERTY_VALUE_SUM,
+        WAS_GROSS_HOUSING_WEALTH,
+    ]:
+        hist = np.histogram(
+            chunk[variable].values,
+            bins=bin_edges,
+            density=True,
+            weights=chunk[WAS_WEIGHT].values,
+        )[0]
         with open("{}-Weighted.csv".format(variable), "w") as f:
-            f.write("# Log {} (lower edge), Log {} (upper edge), Probability\n".format(variable, variable))
-            for element, wealthLowerEdge, wealthUpperEdge in zip(hist, bin_edges[:-1], bin_edges[1:]):
-                f.write("{}, {}, {}\n".format(wealthLowerEdge, wealthUpperEdge, element))
+            f.write(
+                "# Log {} (lower edge), Log {} (upper edge), Probability\n".format(
+                    variable, variable
+                )
+            )
+            for element, wealthLowerEdge, wealthUpperEdge in zip(
+                hist, bin_edges[:-1], bin_edges[1:]
+            ):
+                f.write(
+                    "{}, {}, {}\n".format(wealthLowerEdge, wealthUpperEdge, element)
+                )
 
 # If plotting data and results is required, read model results, histogram data and results and plot them
 if plotResults:
     # Read model results
-    results = readResults(rootResults + r"/test/HousingWealth-run1.csv", start_time, end_time)
+    results = readResults(
+        os.path.join(rootResults, "test", "HousingWealth-run1.csv"),
+        start_time,
+        end_time,
+    )
     # Histogram model results
-    model_hist = np.histogram([x for x in results if x > 0.0], bins=bin_edges, density=False)[0]
+    model_hist = np.histogram(
+        [x for x in results if x > 0.0], bins=bin_edges, density=False
+    )[0]
     model_hist = model_hist / sum(model_hist)
     # Histogram data from WAS
-    WAS_hist = np.histogram(chunk[chunk[variableToPlot] > 0.0][variableToPlot].values, bins=bin_edges,
-                            density=False, weights=chunk[chunk[variableToPlot] > 0.0]["Weight"].values)[0]
+    WAS_hist = np.histogram(
+        chunk[chunk[variableToPlot] > 0.0][variableToPlot].values,
+        bins=bin_edges,
+        density=False,
+        weights=chunk[chunk[variableToPlot] > 0.0][WAS_WEIGHT].values,
+    )[0]
     WAS_hist = WAS_hist / sum(WAS_hist)
     # Plot both model results and data from WAS
-    plt.bar(bin_edges[:-1], height=model_hist, width=bin_widths, align="edge", label="Model results", alpha=0.5,
-            color="b")
-    plt.bar(bin_edges[:-1], height=WAS_hist, width=bin_widths, align="edge", label="WAS data", alpha=0.5, color="r")
+    plt.bar(
+        bin_edges[:-1],
+        height=model_hist,
+        width=bin_widths,
+        align="edge",
+        label="Model results",
+        alpha=0.5,
+        color="b",
+    )
+    plt.bar(
+        bin_edges[:-1],
+        height=WAS_hist,
+        width=bin_widths,
+        align="edge",
+        label="WAS data",
+        alpha=0.5,
+        color="r",
+    )
     # Final plot details
     plt.gca().set_xscale("log")
     plt.xlabel("{}".format(variableToPlot))
     plt.ylabel("Frequency (fraction of cases)")
     plt.legend()
-    plt.title("Distribution of Mark-to-market Net Housing Wealth ({})".format(variableToPlot))
+    plt.title(
+        "Distribution of Mark-to-market Net Housing Wealth ({})".format(variableToPlot)
+    )
     plt.show()
